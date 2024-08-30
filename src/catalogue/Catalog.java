53a68e94b2f6e00a94f9e40a5dee5b4cc081ebc0
AP-JavaCod <megafoh3g@gmail.com>
2024-08-30 21:23:58 +0500

    Changes Catalog.java

--- src/catalogue/Catalog.java
+++ src/catalogue/Catalog.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface Catalog {
 
     void add(Object values);
+    void add(Object[] array);
     <T> List<T> getListObject(Class<T> obj);
     <T> T getObject(Class<T> obj, int index);
     <T> void removeList(Class<T> obj);
