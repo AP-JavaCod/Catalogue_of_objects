d14d5db7af523afba92edce339a8faba1239e161
AP-JavaCod <megafoh3g@gmail.com>
2024-09-09 23:16:03 +0500

    Creation ExtractList.java

---
+++ src/catalogue/ExtractList.java
@@ -0,0 +1,45 @@
+package catalogue;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExtractList implements Extract{
+
+    protected final Map<Class<?>, List<Object>> data = new HashMap<>();
+
+    @Override
+    public Class<?>[] getObjectsClass() {
+        return data.keySet().toArray(new Class[0]);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T[] getObjects(Class<T> obj) {
+        List<T> list = getListObject(obj);
+        return list.toArray((T[]) Array.newInstance(obj, list.size()));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getObject(Class<T> obj, int index) {
+        return (T) getListObject(obj).get(index);
+    }
+
+    @Override
+    public void removeList(Class<?> obj) {
+        data.remove(obj);
+    }
+
+    @Override
+    public void removeObject(Class<?> obj, int index) {
+        getListObject(obj).remove(index);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected <T> List<T> getListObject(Class<?> obj){
+        return (List<T>) data.get(obj);
+    }
+
+}
