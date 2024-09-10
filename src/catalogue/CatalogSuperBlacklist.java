8b79eea10ed7798f194212a3d936064af8b9200e
AP-JavaCod <megafoh3g@gmail.com>
2024-09-10 08:07:31 +0500

    Creation CatalogSuperBlacklist.java

---
+++ src/catalogue/CatalogSuperBlacklist.java
@@ -0,0 +1,36 @@
+package catalogue;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CatalogSuperBlacklist extends CatalogSuperList implements Blacklist{
+
+    private final Set<Class<?>> blacklist = new HashSet<>();
+
+    @Override
+    public void block(Class<?> obj) {
+        blacklist.add(obj);
+    }
+
+    @Override
+    public void blockAll(Class<?>... obj) {
+        Collections.addAll(blacklist, obj);
+    }
+
+    @Override
+    public void unblock(Class<?> obj) {
+        blacklist.remove(obj);
+    }
+
+    @Override
+    public boolean isBlacklist(Class<?> obj) {
+        return blacklist.contains(obj);
+    }
+
+    @Override
+    public Class<?>[] getBlocks() {
+        return blacklist.toArray(new Class[0]);
+    }
+
+}
