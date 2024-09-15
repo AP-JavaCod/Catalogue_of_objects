d950a891910bb2f3295a9e4ed2f09b85fa6e9552
AP-JavaCod <megafoh3g@gmail.com>
2024-09-15 09:34:14 +0500

    Update

--- src/catalogue/ExtractList.java
+++ src/catalogue/ExtractList.java
@@ -72,7 +72,7 @@ public class ExtractList implements Extract, Iterable<ExtractList.DataValues> {
 
         @Override
         public DataValues next() {
-            DataValues val = null;
+            DataValues val;
             if (objects.length > indexObject) {
                 val = new DataValues(classes[indexClass - 1], objects[indexObject]);
                 indexObject++;
@@ -86,6 +86,8 @@ public class ExtractList implements Extract, Iterable<ExtractList.DataValues> {
                 }else {
                     val = new DataValues(classes[indexClass - 1], null);
                 }
+            }else {
+                throw new NullPointerException();
             }
             return val;
         }
